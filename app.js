94a8f8be29995a8ce5e4c505d5f523e16be609de
Tiffany Monroe <hello@tiffanymonroe.com>
2017-10-17 21:48:49 -0700

    added time, basic funtion

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('meow');
+
+let hour = new Date;
+
+const time = hour.getHours();
+
+console.log(time);
+
+if (time < 12 ){
+  console.log('good morning');
+} else {
+  console.log('good day');
+}
